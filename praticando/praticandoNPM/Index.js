f363f563693504fe6416236df69838eb7b3c164f
= <=>
2021-08-06 11:51:41 -0300

    salvando 06/08

--- praticando/praticandoNPM/Index.js
+++ praticando/praticandoNPM/Index.js
@@ -1,15 +1,23 @@
 
 
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+const path = require("path");
+const os = require("os");
 
 
 
 
 
-const cowsay = require("cowsay")
 
-console.log(cowsay.say({
-    text: "Vacas não falam",
-    e : "oo",
-    T : "U "
-}));
+(async () => {
+
+    const alunos = await (await fsPromises.readFile(path.resolve(__dirname, "alunos.json"))).toString()
+    let students = await JSON.parse(alunos)
+    let aprovados = students.filter(aluno => ((aluno.notas[0] + aluno.notas[1] + aluno.notas[2]) / 3) >= 6)
+    for (let i = 0; i < aprovados.length; i++) {
+        aprovados[i].media = Math.round((aprovados[i].notas[0] + aprovados[i].notas[1] + aprovados[i].notas[2]) / 3)
+    }
+    fsPromises.writeFile(path.resolve(__dirname, "aprovados.json"), JSON.stringify(aprovados))
+})();
 
